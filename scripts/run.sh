87631de2bfc080545a48bda5398156004602d664
CaduRomaniello <cadu_romaniello@hotmail.com>
2022-06-01 15:56:29 -0300

    output

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+cd ../src
+make run SIZE=30 CAP=50 ALG=I
+make run SIZE=30 CAP=50 ALG=R
+make run SIZE=30 CAP=50 ALG=B
+
+
+make run SIZE=30 CAP=100 ALG=I
+make run SIZE=30 CAP=100 ALG=R
+make run SIZE=30 CAP=100 ALG=B
+
+make run SIZE=30 CAP=10000 ALG=I
+make run SIZE=30 CAP=10000 ALG=R
+make run SIZE=30 CAP=10000 ALG=B
