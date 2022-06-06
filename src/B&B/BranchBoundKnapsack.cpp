f048407a09012a8f85d2b8512399438b84b2ddc8
Vinicius Verona <verona.projects@tutanota.com>
2022-06-05 21:42:17 -0300

    Fix B&B print comments

--- src/B&B/BranchBoundKnapsack.cpp
+++ src/B&B/BranchBoundKnapsack.cpp
@@ -8,7 +8,6 @@ BranchBoundKnapsack::BranchBoundKnapsack(int nitems, int weight) {
     this->bestAssignment = items;
     this->value = 0;
     this->bestValue = 0;
-
 }
 BranchBoundKnapsack::~BranchBoundKnapsack() {
     this->items.clear();
@@ -17,9 +16,8 @@ BranchBoundKnapsack::~BranchBoundKnapsack() {
 }
 
 void BranchBoundKnapsack::initialSolution(const Problem* instance) {
-
     vector<pair<double, int>> proportion;
-    for (int i = 1; i <= instance->getSize(); i++){
+    for (int i = 1; i <= instance->getSize(); i++) {
         Item* item = instance->getItem(i - 1);
         pair<double, int> x;
         x.first = (double)item->value / (double)item->weight;
@@ -30,41 +28,37 @@ void BranchBoundKnapsack::initialSolution(const Problem* instance) {
 
     this->valueProportion = proportion;
     sort(proportion.begin(), proportion.end(), greater<pair<double, int>>());
-    
+
     int capacity = instance->getCapacity();
 
-    for (int i = 1; i <= (int) proportion.size(); i++){
+    for (int i = 1; i <= (int)proportion.size(); i++) {
         int position = proportion[i - 1].second;
         Item* item = instance->getItem(position);
-        
-        if (capacity - item->weight >= 0){
+
+        if (capacity - item->weight >= 0) {
             this->bestAssignment[position] = 1;
             this->weight += item->weight;
             this->bestValue += item->value;
             capacity -= item->weight;
 
-        } else{
+        } else {
             this->bestAssignment[position] = 0;
         }
     }
-
 }
 
 bool BranchBoundKnapsack::isComplete(vector<int> items) {
-    
     for (int i : items) {
         if (i == -1) return false;
     }
 
     return true;
-
 }
 
 bool BranchBoundKnapsack::isConsistent(const Problem* instance, vector<int> items) {
-    
     int totalWeight = 0;
     int counter = 0;
-    
+
     for (int i : items) {
         if (i == 1) {
             totalWeight += instance->getItem(counter)->weight;
@@ -78,13 +72,11 @@ bool BranchBoundKnapsack::isConsistent(const Problem* instance, vector<int> item
     }
 
     return true;
-
 }
 
 bool BranchBoundKnapsack::isPromissing(const Problem* instance, vector<int> items, int idx, int value, int weight) {
-
     double nValue = value;
-    
+
     if (items[idx]) {
         Item* item = instance->getItem(idx);
         weight += item->weight;
@@ -96,72 +88,67 @@ bool BranchBoundKnapsack::isPromissing(const Problem* instance, vector<int> item
 
     vector<int> nitems = items;
 
-    for (auto it = proportion.begin() + idx + 1; it != proportion.end(); ++it){
+    for (auto it = proportion.begin() + idx + 1; it != proportion.end(); ++it) {
         Item* item = instance->getItem(it->second);
-        if (weight + item->weight <= instance->getCapacity()){
+        if (weight + item->weight <= instance->getCapacity()) {
             weight += item->weight;
             nValue += item->value;
             nitems[it->second] = 1;
-        
-        } else{
+
+        } else {
             int remainder = instance->getCapacity() - weight;
             weight += remainder;
-            nValue += (remainder * it->first); 
+            nValue += (remainder * it->first);
             break;
         }
     }
 
-    if (this->bestValue < nValue) return true;
-    else return false;
-
+    if (this->bestValue < nValue)
+        return true;
+    else
+        return false;
 }
 
 void BranchBoundKnapsack::solve(const Problem* instance) {
-
     this->initialSolution(instance);
 
-    if (!(this->isComplete(this->bestAssignment) && this->isConsistent(instance, this->bestAssignment))){
+    if (!(this->isComplete(this->bestAssignment) && this->isConsistent(instance, this->bestAssignment))) {
         cout << "Solução errada, revise o codigo!!!" << endl;
     }
 
     this->weight = 0;
     this->_solve(instance, this->items, 0, this->value, this->weight);
-
 }
 
 void BranchBoundKnapsack::_solve(const Problem* instance, vector<int> items, int item, int value, int weight) {
-
-    if (this->isComplete(items)){
-
+    if (this->isComplete(items)) {
         this->bestAssignment = items;
         this->bestValue = value;
 
-    } else{
+    } else {
         items[item] = 0;
-        if (this->isConsistent(instance, items) && this->isPromissing(instance, items, item, value, weight)){
+        if (this->isConsistent(instance, items) && this->isPromissing(instance, items, item, value, weight)) {
             this->_solve(instance, items, item + 1, value, weight);
         }
 
         items[item] = 1;
-        if (this->isConsistent(instance, items) && this->isPromissing(instance, items, item, value, weight)){
-            this->_solve(instance, items, item + 1, value + instance->getItem(item)->value, weight + instance->getItem(item)->weight);
+        if (this->isConsistent(instance, items) && this->isPromissing(instance, items, item, value, weight)) {
+            this->_solve(instance, items, item + 1, value + instance->getItem(item)->value,
+                         weight + instance->getItem(item)->weight);
         }
 
         items[item] = -1;
     }
-
 }
-
 
 void BranchBoundKnapsack::printSolution(const Problem* instance) {
     cout << "The total value for the given instance is: " << this->bestValue << endl;
     cout << "The items in the backpack are: [ ";
 
-    for (int i = this->bestAssignment.size(); i > 0; i--){
+    for (int i = this->bestAssignment.size(); i > 0; i--) {
         if (this->bestAssignment[i - 1] == 1) {
             cout << i << " ";
         }
     }
     cout << "]" << endl;
-
 }
